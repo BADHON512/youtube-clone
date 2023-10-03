fc8251d2d98448e7aff2aac9980b3fe5deea430f
BADHON <mylaptop512@gmail.com>
2023-10-03 23:31:41 +0600

    first commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,19 @@
+"use client"
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+export default function Header() {
+const [time, settime] = useState(false)
+console.log(time)
+useEffect(() => {
+  if (time) {
+    toast.success("ok")
+  }else{}
+
+ 
+},[time])
+
+  return (
+    <div onClick={()=>settime(true)}>Header</div>
+  )
+}
